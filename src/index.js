5c7b7046d48d36127cbe08b89038bc364fd1ce79
Shashank K R <krshashank1118@gmail.com>
2024-02-17 23:44:17 +0530

    Middlewares - app.use()

--- src/index.js
+++ src/index.js
@@ -1,10 +1,25 @@
 import dotenv from 'dotenv';
 import dbConnect from './db/index.js';
+import { app } from './app.js';
 // require("dotenv").config({path: './env'})        // method 2
 
 dotenv.config({
     path : './env'
 });
 
-dbConnect();
+dbConnect()
+.then(()=>{
+
+    app.on('error', (err)=> {       // additional
+        console.error(err);
+        throw err
+    })
+
+    app.listen( preocess.env.PORT || 8000 , ()=> {
+        console.log(`Server is running on port ${process.env.PORT}`)
+    })
+})
+.catch((error)=>{
+    console.error("\nMongoDB connection Failed", error);
+})
 
