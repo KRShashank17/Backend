d9a193d61e2feead914523c408ccefe571092111
Shashank K R <krshashank1118@gmail.com>
2024-02-22 00:25:36 +0530

    unlinkSync - deletes from public/temp

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -16,7 +16,7 @@ const uploadOnCloudinary = async (localFilePath) => {
             resource_type: "auto" 
         })
         // console.log("URL : " , response.url);
-        // fs.unlinkSync(localFilePath);
+        fs.unlinkSync(localFilePath);           //! if successfully uploaded to cloudinary - delete from ./public/temp
         return response;
     
     } catch (error) {
