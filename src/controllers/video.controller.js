524e2c7dfb6e2499144a091187c1a99676cf9d98
Shashank K R <krshashank1118@gmail.com>
2024-03-20 23:08:42 +0530

    publishing a Video

--- src/controllers/video.controller.js
+++ src/controllers/video.controller.js
@@ -86,6 +86,54 @@ const getAllVideos = asyncHandler(async (req, res) => {
 })
 
 
+// get video, upload on Cloudinary
+const publishAVideo = asyncHandler(async (req, res) => {
+    const {title, description} = req.body;
+    if ([title, description].some((field) => field?.trim() === "")){
+        throw new ApiError(400, "All fields are required");
+    }
+
+    const videoFileLocalPath = req.files?.videoFile[0].path;
+    const thumbnailLocalPath = req.files?.thumbnail[0].path;
+
+    if (!videoFileLocalPath || !thumbnailLocalPath){
+        throw new ApiError(400, "All files are required");
+    }
+
+    const videoFile = await uploadOnCloudinary(videoFileLocalPath);
+    const thumbnail = await uploadOnCloudinary(thumbnailLocalPath);
+
+    if (!videoFile || !thumbnail){
+        throw new ApiError(500, "Files not Found - Failed to upload video");
+    }
+
+    const video = await Video.create({
+        title,
+        description,
+        duration : videoFile.duration,
+        videoFile: {
+            url : videoFile.url,
+            public_id : videoFile.public_id
+        },
+        thumbnail : {
+            url : thumbnail.url,
+            public_id : thumbnail.public_id
+        },
+        owner : req.user?._id,
+        isPublished : false
+    })
+
+    const videoUploaded = await Video.findById(video._id);
+    if (!videoUploaded){
+        throw new ApiError(500, "Failed to upload video - Something went wrong");
+    }
+
+    return res.status(200)
+             .json(new ApiResponse(200, videoUploaded, "Video Uploaded Successfully"));
+})
+
+
 export {
-    getAllVideos
+    getAllVideos,
+    publishAVideo
 }
