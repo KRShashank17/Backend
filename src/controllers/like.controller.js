cb2e3891ba16e017694ddad193faff38e7de067c
Shashank K R <krshashank1118@gmail.com>
2024-03-29 22:33:50 +0530

    Fetch Liked Videos added

--- src/controllers/like.controller.js
+++ src/controllers/like.controller.js
@@ -83,8 +83,74 @@ const toggleCommentLike = asyncHandler(async (req, res) => {
               .json(new ApiResponse(200, { isLiked : true}))
 })
 
+const getLikedVideos = asyncHandler(async(req,res)=>{
+    const likedVideoAggregate = await Like.aggregate([
+        {
+            $match : {
+                likedBy: new mongoose.Types.ObjectId(req.user?._id)
+            }
+        },
+        {
+            $lookup: {
+                from: "videos",
+                localField: "video",
+                foreignField: "_id",
+                as: "likedVideo",
+
+                pipeline: [
+                    {
+                        $lookup: {
+                            from: "users",
+                            localField: "owner",
+                            foreignField: "_id",
+                            as: "ownerDetails",
+                        }
+                    },
+                    {
+                        $unwind: "$ownerDetails"
+                    }
+                ]
+            }
+        },
+        {
+            $unwind: "$likedVideo"
+        },
+        {
+            $sort: {
+                createdAt: -1
+            }
+        },
+        {
+            $project : {
+                _id: 0,
+                likedVideo: {
+                    _id: 1,
+                    "videoFile.url": 1,
+                    "thumbnail.url": 1,
+                    owner: 1,
+                    title: 1,
+                    description: 1,
+                    views: 1,
+                    duration: 1,
+                    createdAt: 1,
+                    isPublished: 1,
+                    ownerDetails: {
+                        username: 1,
+                        fullName: 1,
+                        "avatar.url": 1,
+                    }
+                }
+            }
+        }
+    ])
+
+    return res.status(200)
+              .json(new ApiResponse(200, likedVideoAggregate, "Liked Videos fetched Successfully"))
+})
+
 export {
     toggleVideoLike,
     toggleTweetLike,
-    toggleCommentLike
+    toggleCommentLike,
+    getLikedVideos
 }
