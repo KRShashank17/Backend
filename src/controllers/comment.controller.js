249b5be5b896431a184675fa994d3b98ebf52c4d
Shashank K R <krshashank1118@gmail.com>
2024-04-01 22:47:04 +0530

    Delete Comment

--- src/controllers/comment.controller.js
+++ src/controllers/comment.controller.js
@@ -3,6 +3,8 @@ import { asyncHandler } from "../utils/asyncHandler";
 import { ApiResponse } from "../utils/apiResponse";
 import { ApiError } from "../utils/ApiError";
 import { Video } from "../models/video.model";
+import {Comment} from "../models/comment.model";
+import { Like } from "../models/like.model";
 
 const addComment = asyncHandler(async (req, res) => {
     const {videoId} = req.params;
@@ -30,6 +32,30 @@ const addComment = asyncHandler(async (req, res) => {
               .json(new ApiResponse(200, comment, "Comment added successfully"))
 })
 
+const deleteComment = asyncHandler(async(req, res) => {
+    const {commentId} = req.params;
+    const comment = await Comment.findById(commentId);
+
+    if (!comment){
+        throw new ApiError(404, "Comment not found");
+    }
+
+    if (comment?.owner.toString() !== req.user?._id.toString()){
+        throw new ApiError(401, "Unauthorized request");
+    }
+
+    await Comment.findByIdAndDelete(commentId);
+
+    await Like.deleteMany({
+        comment : commentId,
+        likedBy : req.user
+    })
+
+    return res.status(200)
+              .json(new ApiResponse(200, {commentId},"Comment deleted successfully"))
+})
+
 export {
-    addComment
+    addComment,
+    deleteComment
 }
